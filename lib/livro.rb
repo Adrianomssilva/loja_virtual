62a5cb82991c22ff07aade7c642377f12bcbcc60
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-11 19:01:29 -0300

    add biblioteca para colocar os livros

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,14 +1,30 @@
+# class Livro
+#    attr_accessor :preco
+#    #  Podemos utilizar o preco = para alterar o valor do livro e preco para retornar a o valor atual
+
+#    def initialize (autor, isbn = "1" , numero_de_paginas, preco)
+#       @autor = autor
+#       @isbn = isbn
+#       @numero_de_paginas = numero_de_paginas
+#       @preco = preco
+#    end
+#    def escrever
+#      puts "Autor: #{@autor}, Isbn: #{@isbn}, Pag: #{@numero_de_paginas}, Preço: #{preco}"      
+#    end
+# end
+
 class Livro
    attr_accessor :preco
 
-   def initialize (autor,isbn = "1" , numero_de_paginas, preco)
-      @autor = autor
-      @isbn = isbn
-      @numero_de_paginas = numero_de_paginas
-      @preco = preco
+   def initialize(autor, isbn = "1", numero_de_paginas, preco)
+   @autor = autor
+   @isbn = isbn
+   @numero_de_paginas = numero_de_paginas
+   @preco = preco
    end
-   def to_s
-      "Autor: #{@autor}, Isbn: #{@isbn}, Pag: #{@numero_de_paginas}"      
-   end
-end
 
+   def to_s
+   "Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+   end
+   
+end
--- lib/teste.rb
+++ lib/teste.rb
@@ -1,9 +1,30 @@
 require_relative "livro.rb"
+require_relative "biblioteca.rb"
 
-# Livro.new "Adriano Silva", 200
 
-teste_e_design = Livro.new("Mauricio Aniche", "123454", 247,200)
-web_design_responsivo = Livro.new("Tárcio Zemel", "452565", 321,300)
+# biblioteca = Biblioteca.new
+# teste_e_design = Livro.new "Mauricio Aniche", "123454", 247, 200
+# web_design_responsivo = Livro.new "Tárcio Zemel", "452565", 321, 300 
+# # teste_e_design.preco = 80
+# # biblioteca = Biblioteca.new
 
-puts teste_e_design.preco
-puts web_design_responsivo.preco
+
+
+# biblioteca.adiciona (teste_e_design)
+# biblioteca.adiciona (web_design_responsivo)
+
+
+# puts biblioteca.livros
+biblioteca = Biblioteca.new
+
+teste_e_design = Livro.new("Mauricio Aniche", "123454", 247,69.4)
+web_design_responsivo = Livro.new("Tárcio Zemel", "452565", 321, 85.6)
+
+biblioteca.adiciona teste_e_design
+biblioteca.adiciona web_design_responsivo
+
+# puts biblioteca.livros
+
+for livro in biblioteca.livros do
+p livro.preco
+end
