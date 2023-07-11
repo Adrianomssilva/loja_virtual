0149c92ce97624f42ee3de86864737664fd61bfe
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-11 16:14:59 -0300

    Add variável preço

--- lib/teste.rb
+++ lib/teste.rb
@@ -2,8 +2,8 @@ require_relative "livro.rb"
 
 # Livro.new "Adriano Silva", 200
 
-teste_e_design = Livro.new("Mauricio Aniche", "123454", 247)
-web_design_responsivo = Livro.new("Tárcio Zemel", "452565", 321)
+teste_e_design = Livro.new("Mauricio Aniche", "123454", 247,200)
+web_design_responsivo = Livro.new("Tárcio Zemel", "452565", 321,300)
 
-puts teste_e_design
-puts web_design_responsivo
+puts teste_e_design.preco
+puts web_design_responsivo.preco
